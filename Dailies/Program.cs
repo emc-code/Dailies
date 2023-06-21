c337d0e357f208c05cca71f5da7e60d8a0489ccf
Evgenii Chavkin <a110589@yandex.ru>
2023-06-21 14:12:20 +0400

    feat: add Hello World

--- Dailies/Program.cs
+++ Dailies/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+var discordSocketClient = new DiscordSocketClient();
+await discordSocketClient.LoginAsync(TokenType.Bot, "MTEyMTAxNjM3Mjc3NjkyNzMwMw.GMFRnq.FoxJtOIL0DpZMO4gXuQaIlBcumkmBa72yxhgDE");
+await discordSocketClient.StartAsync();
+
+discordSocketClient.Log += LogMessage;
+
+await Task.Delay(-1);
+
+static Task LogMessage(LogMessage arg)
+{
+    Console.WriteLine(arg);
+    return Task.CompletedTask;
+}
