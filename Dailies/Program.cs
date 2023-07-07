262e3dcd126877964fc8941378395d84aa5043a8
Evgenii Chavkin <a110589@yandex.ru>
2023-07-07 18:14:20 +0400

    draft

--- Dailies/Program.cs
+++ Dailies/Program.cs
@@ -1,18 +1,24 @@
 ﻿using Discord;
 using Discord.WebSocket;
 
-const string OnOfftSignal = "!1"; // stop\start - при получении сигнала чекнет и начнет чекать при каждом событие на сервере
+const string StopSignal = "sss";    // stop
+const string CheckSignal = "ccc";   // ccc
+const int StartHour = 18;
+const int StartMinute = 00;
+
 bool checkUsers = true;
 
 Dictionary<string, string> allUsers = new Dictionary<string, string> { { "ds_emc", "Админ Админович"},
                                                                        { "test", "Тест Тестович" } };
 
 DiscordSocketConfig discordSocketConfig = new DiscordSocketConfig();
-discordSocketConfig.GatewayIntents = GatewayIntents.All;
+discordSocketConfig.GatewayIntents = GatewayIntents.MessageContent;
 
 var discordSocketClient = new DiscordSocketClient();
 
 discordSocketClient.MessageReceived += DiscordSocketClient_MessageReceived;
+
+
 
 await discordSocketClient.LoginAsync(TokenType.Bot, "MTEyMTAxNjM3Mjc3NjkyNzMwMw.GycfOT.0i4fhb9rQa4eVnkS41S5-eZKHrc6JR-H2O-DvQ");
 await discordSocketClient.StartAsync();
@@ -21,27 +27,32 @@ Console.Read();
 
 Task DiscordSocketClient_MessageReceived(SocketMessage arg)
 {
-    string qwe = arg.Content.Trim();
-    arg.Channel.SendMessageAsync("1234123 41234" + arg.Content);
-
-    return Task.CompletedTask;
 
     if (arg.Channel.Name == "dailies")
     {
         if (arg.Author.IsBot == false)
         {
-            /* if (arg.Content.ToUpper() == OnOfftSignal.ToUpper())
-                 checkUsers = !checkUsers;
+            if (arg.Content.ToUpper() == StopSignal.ToUpper())
+                checkUsers = false;
 
-             arg.Channel.SendMessageAsync(arg.Content);
-
-             if (checkUsers)
-            */
-            Check(arg);
+            if (arg.Content.ToUpper() == CheckSignal.ToUpper())
+                Check(arg);
         }
     }
 
     return Task.CompletedTask;
+}
+
+string GetLastMessage(SocketMessage arg)
+{
+    if (arg.Channel is SocketTextChannel textChannel)
+    {
+        var lastMessage = textChannel.GetMessagesAsync(10).FlattenAsync().Result.ToList().Where(x => x.Author.IsBot == false).Select(x => x.Content).ToList();
+
+        return "123";
+    }
+
+    return string.Empty;
 }
 
 void Check(SocketMessage arg)
